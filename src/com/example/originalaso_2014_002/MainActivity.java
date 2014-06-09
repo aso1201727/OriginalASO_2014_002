2a7312eb49b01042aa25bf7dfa9b0f0d963c23d6
student <student@PC111S09.fgakusei.local>
2014-06-09 15:17:58 +0900

    アップデート！

--- src/com/example/originalaso_2014_002/MainActivity.java
+++ src/com/example/originalaso_2014_002/MainActivity.java
@@ -1,63 +1,70 @@
 package com.example.originalaso_2014_002;
 
+
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements OnClickListener {
+	
+	// クラスで使用するそれぞれのインスタンスの宣言
+	SQLiteDatabase sdb = null;
+	MySQLiteOpenHelper helper = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+	}
+	
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		super.onResume();
+		// ボタンをIDで探してボタンを作る
+		Button BtnCheck = (Button)findViewById(R.id.BtnCheck);
+		// ボタン変数をリスナーを登録する。
+		BtnCheck.setOnClickListener(this);
+		
+		// ボタンをIDで探してボタンを作る
+		Button BtnTouroku = (Button)findViewById(R.id.BtnTouroku);
+		// ボタン変数をリスナーを登録する。
+		BtnCheck.setOnClickListener(this);
+		
+		if(sdb == null) {
+			helper = new MySQLiteOpenHelper(getApplicationContext());
+		} try {
+			sdb = helper.getWritableDatabase();
+		} catch(SQLiteException e) {
+			//異常終了
+			return;
 		}
+				
+		// ボタンをIDで探してボタンを作る
+		Button BtnMente = (Button)findViewById(R.id.BtnMente);
+		// ボタン変数をリスナーを登録する。
+		BtnCheck.setOnClickListener(this);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
+		switch(v.getId()){
+			case R.id.BtnCheck:
+				startActivity(new Intent(MainActivity.this, HyoujiActivity.class));
+				break;
+			case R.id.BtnTouroku:
+				
+				break;
+			case R.id.BtnMente:
+				startActivity(new Intent(MainActivity.this, HyoujiActivity.class));
+				break;
 		}
 	}
 
